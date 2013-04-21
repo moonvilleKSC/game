0ef76484fb89b3d7b26927b6efdd7b536659dc10
Kevin D. French <fnivek@gmail.com>
2013-04-20 23:19:32 -0400

    Initial Graphic class complotted, and graphics Managaer added

--- Moonville/src/com/ksc/moonville/Graphic.java
+++ Moonville/src/com/ksc/moonville/Graphic.java
@@ -1,18 +1,33 @@
 package com.ksc.moonville;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Graphic {
-	//variables
-	float xPos, yPos;
-	
-	Depth depth;
-	
-	//drawBitmap
-	
-	void Draw(Canvas canvas)
-	{
-		
-	}
-	
+    //variables
+    float xPos, yPos;
+    
+    Depth depth;
+    
+    private Bitmap image;
+    
+    /*
+     * Constructors
+     */
+    public Graphic(Bitmap bitmap)
+    {
+        image = bitmap;
+    }
+    
+    public Graphic(Bitmap bitmap, float x, float y)
+    {
+        
+    }
+    
+    //draw function
+    void Draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image, xPos, yPos, null);
+    }
+    
 }
---
+++ Moonville/src/com/ksc/moonville/GraphicsManager.java
@@ -0,0 +1,13 @@
+package com.ksc.moonville;
+
+public class GraphicsManager {
+	//variables
+	
+	
+	
+	//Constructors
+	private GraphicsManager()
+	{
+		// to twart any one from making one (at least easily)
+	}
+}
